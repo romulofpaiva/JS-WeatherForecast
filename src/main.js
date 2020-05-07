576272cf5237c95f2bba7bd096c330a96f4156d0
Romulo Paiva <romulofpaiva@gmail.com>
2020-05-07 19:53:29 -0300

    Commit com arquivos do projeto

---
+++ src/main.js
@@ -0,0 +1,166 @@
+// ATENÇÃO
+// Para funcionar será necessário:
+// 1º: Crie uma conta e um token em https://advisor.climatempo.com.br/
+// 2º: Crie um projeto na conta.
+// 3º: Inicialize a variável tokenClimaTempo com o disponibilizado para o token do projeto.
+
+const tokenClimaTempo = "";
+
+var states = [];
+var cities = [];
+
+
+function countrySynopticAnalysis() {
+  $.getJSON(
+    `http://apiadvisor.climatempo.com.br/api/v1/anl/synoptic/locale/BR?token=${tokenClimaTempo}`,
+    "",
+    function(data, status, xhr){
+      if(status !== 'success'){
+        alert('Não foi possível obter a análise sinótica meteorológica do paí­s.');
+        return;
+      }
+
+      $("#countrySynopticAnalysis").append( data[0].text );
+    });
+};
+
+countrySynopticAnalysis();
+
+async function findCityId(cityName, UF) {
+  if(cityName == null)
+    return;
+
+  var cityId = 0;
+
+  await $.getJSON(
+    `http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=${cityName.toLowerCase()}&state=${UF}&token=${tokenClimaTempo}`,
+    "",
+    function(data, status, xhr){
+      if(status !== 'success'){
+        alert('Não foi possível obter as informações da cidade.');
+        return;
+      }
+
+      cityId = data.find( it => it.name.toUpperCase() === cityName.toUpperCase() ).id;
+    }
+  );
+
+  return cityId;
+};
+
+function registerCity(cityId) {
+  if(cityId == null)
+    return;
+
+  $.ajax({
+      url: `http://apiadvisor.climatempo.com.br/api-manager/user-token/${tokenClimaTempo}/locales`,
+      method: "PUT",
+      contentType: 'application/x-www-form-urlencoded',
+      data: `localeId[]=${cityId}`,
+      success: function( msg ) {
+        console.log( "Data Saved: " + msg );
+      },
+      error: function( jqXHR, textStatus ) {
+        console.log( "Request failed: " + textStatus );
+      }
+    });
+};
+
+async function currentWeatherByCity() {
+  var cityName = $("#city").val();
+  var ufId = $("select#state").val();
+  var ufSigla = states.find( it => it.id == ufId ).sigla;
+  var cityId = await findCityId(cityName, ufSigla);
+
+  if(cityId == null){
+    alert('Cidade não localizada.');
+    return;
+  }
+
+  // await registerCity(cityId);
+
+  $.getJSON(
+    `http://apiadvisor.climatempo.com.br/api/v1/weather/locale/${cityId}/current?token=${tokenClimaTempo}`,
+    "",
+    function(data, status, xhr){
+      if(status !== 'success'){
+        alert('Não foi possível obter as informações meteorológicas da cidade.');
+        return;
+      }
+
+      const {condition, date, humidity, sensation, temperature} = data.data;
+      const options = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+
+      const event = new Date(date);
+
+      $("#analysis").text('');
+      $("#analysis").append( `Condições Climáticas às ${event.toLocaleDateString('pt-BR', options)}</br>` );
+      $("#analysis").append( `${condition}</br>` );
+      $("#analysis").append( `Umidade: ${humidity}</br>` );
+      $("#analysis").append( `Sensação Térmica: ${sensation}</br>` );
+      $("#analysis").append( `Temperatura: ${temperature} °C</br>` );
+      // $("div#cityAnalysis").attr('hidden', false);
+    }
+  );
+}
+
+function fillState() {
+  $.getJSON(
+    "https://servicodados.ibge.gov.br/api/v1/localidades/estados", 
+    "",
+    function(data, status, xhr){
+      if(status !== 'success'){
+        alert('Não foi possível obter a lista de Estados.');
+        return;
+      }
+
+      states = data.map( it => ({id: it.id, sigla: it.sigla}) );
+
+      data.sort( (a, b) => (a.sigla.toUpperCase() < b.sigla.toUpperCase() ? -1 : 1) );
+      data.forEach(item => $("select#state").append( new Option(item.sigla, item.id) ) );
+    });
+};
+
+fillState();
+
+function fillCity() {
+  var UF = $("select#state").val();
+  if(UF == null)
+    return;
+
+  $.getJSON(
+    `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${UF}/municipios`,
+    "",
+    function(data, status, xhr){
+      if(status !== 'success'){
+        alert('Não foi possível obter a lista de Cidades.');
+        return;
+      }
+
+      cities = data.map( it => ({id: it.id, nome: it.nome}) );
+    });
+};
+
+function findCity() {
+  var city = $("#city").val();
+
+  if(city == null || city.length < 3)
+    return;
+
+  $("ul#listCities").empty();
+
+  cities.forEach( it => {
+    if(it.nome.toUpperCase().substring(0, city.length) === city.toUpperCase()) {
+      $("ul#listCities").append( `<li><a href="#" onclick="selectCity(event)" id="${it.id}">${it.nome}</a></li>` );
+    }
+  });
+};
+
+function selectCity(event) {
+  let cityId = event.target.id;
+  let cityName = event.target.innerText;
+  $("#city").val( cityName );
+  // $("#city").attr( 'cityid', cityId );
+  $("ul#listCities").empty();
+  currentWeatherByCity();
+};
